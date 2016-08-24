20ad31c1ece855a08656f7b9a51c1f3a880b713b
Andreas <mail@andreaswurm.at>
2016-08-24 12:23:44 +0200

    added basic layout
    added the basic layout stuff for the frontend

---
+++ budgets.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <!-- include html header -->
+  <?php include('./head.inc');?>
+
+  <body>
+    <div class="container">
+      <!-- include main navigation -->
+      <?php include('./nav.inc');?>
+      <h1>Budgets</h1>
+  </div>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+  </script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <!-- include html header -->
+  <?php include('./head.inc');?>
+
+  <body>
+    <div class="container">
+      <!-- include main navigation -->
+      <?php include('./nav.inc');?>
+      <div class="jumbotron">
+        <h1>Hello, world!</h1>
+        <p>This is the simple "Hello World" page.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs »</a>
+        </p>
+      </div>
+  </div>
+  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
+  </script>
+  </body>
+</html>
