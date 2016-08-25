249c5e22f5cc792d982e6c26d6b71c3d5c7c5871
Andreas <mail@andreaswurm.at>
2016-08-25 10:18:31 +0200

    added AdminLTE support

--- budgets.php
+++ budgets.php
@@ -1,17 +1,47 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
   <!-- include html header -->
   <?php include('./head.inc');?>
+</head>
 
   <body>
     <div class="container">
       <!-- include main navigation -->
       <?php include('./nav.inc');?>
       <h1>Budgets</h1>
-  </div>
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
-  </script>
+      <div class="panel panel-default">
+        <div class="panel-heading clearfix">
+          <h3 class="panel-title pull-left">BMW</h3>
+          <button type="button" class="btn btn-default btn-sm pull-right">
+            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+          </button>
+        </div>
+        <div class="panel-body">
+          <div class="progress">
+            <div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+              <span class="sr-only">40% Complete (success)</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading clearfix">
+          <h3 class="panel-title pull-left">Siegfried Ludwig-Platz</h3>
+          <button type="button" class="btn btn-default btn-sm pull-right">
+            <span class="glyphicon glyphicon-cog" aria-hidden="true"></span>
+          </button>
+        </div>
+        <div class="panel-body">
+          <div class="progress">
+            <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+              <span class="sr-only">80% Complete (danger)</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- include scripts -->
+    <?php include('./scripts.inc');?>
   </body>
 </html>
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-  <!-- include html header -->
-  <?php include('./head.inc');?>
-
+  <head>
+    <!-- include html header -->
+    <?php include('./head.inc');?>
+  </head>
   <body>
     <div class="container">
       <!-- include main navigation -->
@@ -10,14 +11,9 @@
       <div class="jumbotron">
         <h1>Hello, world!</h1>
         <p>This is the simple "Hello World" page.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs »</a>
-        </p>
       </div>
-  </div>
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <!-- Latest compiled and minified JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
-  </script>
+    </div>
+    <!-- include scripts -->
+    <?php include('./scripts.inc');?>
   </body>
 </html>
